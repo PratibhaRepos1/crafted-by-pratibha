eb0746d50ea92d8aa7b3efa7677e1ff3c928cfe9
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2024-10-28 21:49:32 +0700

    added the about-me section

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient, HttpClientModule  } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit  {
+  aboutMe: string | undefined;
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.http.get<{ aboutMe: string }>('../../../assets/data/about-me.json').subscribe(data => {
+      this.aboutMe = data.aboutMe;
+    });
+  }
 }
